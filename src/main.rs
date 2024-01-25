1d84d4d819be1236eeca951bfe60c07cb67ff005
Justin Lafferty <justin.lafferty@kw.com>
2024-01-24 19:53:53 -0500

    render blank screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::color;
+use macroquad::window::{clear_background, Conf, next_frame};
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Snake".to_owned(),
+        fullscreen: false,
+        window_resizable:  false,
+        window_width: 800,
+        window_height: 600,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(color::BLACK);
+        next_frame().await;
+    }
 }
