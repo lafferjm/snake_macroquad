c6eb261627aeb5f5156c02f40a4a8a5f95ed93f8
Justin Lafferty <justin.lafferty@kw.com>
2024-01-24 19:58:58 -0500

    render snake head

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use macroquad::color;
-use macroquad::window::{clear_background, Conf, next_frame};
+use macroquad::math::{Vec2, vec2};
+use macroquad::shapes::draw_rectangle;
+use macroquad::window::{clear_background, Conf, next_frame, screen_height, screen_width};
 
 fn window_conf() -> Conf {
     Conf {
@@ -12,10 +14,33 @@ fn window_conf() -> Conf {
     }
 }
 
+struct Snake {
+    position: Vec2
+}
+
+impl Snake {
+    fn new() -> Self {
+        let x = screen_width() / 2.0 - 20.0;
+        let y = screen_height() / 2.0 - 20.0;
+        Snake {
+            position: vec2(x, y),
+        }
+    }
+
+    fn draw(&self) {
+        draw_rectangle(self.position.x, self.position.y, 20.0, 20.0, color::GREEN);
+    }
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
+    let snake = Snake::new();
+
     loop {
         clear_background(color::BLACK);
+
+        snake.draw();
+
         next_frame().await;
     }
 }
